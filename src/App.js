394a28cd25931718a3d4ed6776b0fe60436dea93
César Gabriel Suárez <suarezcesar89@gmail.com>
2022-01-23 23:47:41 -0300

    Agregado de NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Edicion de la linea de React para el ejercicio.
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <NavBar />
+      </div>
+    </>
   );
 }
 
