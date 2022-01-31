e036dd64f8489995f7c4b2a8d50088cefad84608
César Gabriel Suárez <suarezcesar89@gmail.com>
2022-01-31 19:46:40 -0300

    Counter V1

---
+++ src/components/cards/ItemListContainer.js
@@ -0,0 +1,11 @@
+import "../../App.css";
+
+function itemListContainer(props) {
+  return (
+    <h1 class="firstTitle">
+      {props.greetings} This is where products will be! :D
+    </h1>
+  );
+}
+
+export default itemListContainer;
